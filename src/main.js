f1415335d3144ceb9a409e35c644d732162ce3f3
Dmitriy0212 <demarr68@gmail.com>
2026-07-04 07:38:42 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import { initSwiper } from './js/gallery.js';
 import { initReviewsSwiper } from './js/reviews.js';
-
-function init() {
-  document.addEventListener('DOMContentLoaded', async () => {
-    await initSwiper();
-    await initReviewsSwiper();
-  });
-}
-init();
+initSwiper();
+initReviewsSwiper();
