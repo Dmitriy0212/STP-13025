e02383bba7631fa5ad8d99f311affb6b86d14d06
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-07-08 14:45:49 +0300

    fix burger menu

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { initSwiper } from './js/gallery.js';
 import { initReviewsSwiper } from './js/reviews.js';
 import './js/faqs.js';
 import { initHowToPlaySwiper } from './js/howtoplay.js';
+import './js/burgerMenu.js';
 
 initSwiper();
 initReviewsSwiper();
